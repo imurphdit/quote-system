b5b600de16504ffd1abe85a762b4a977075fb8de
imurphdit <masinimurphy@icloud.com>
2025-01-22 20:38:07 -0500

    added ability to select colors kinda

--- src/components/Colors.jsx
+++ src/components/Colors.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types'
 
-const Colors = ({ colors }) => {
+const Colors = ({ colors, onClick }) => {
   return (
     <div className="colors-grid">
         {colors.map((color) => (
-            <div className='color' key={color}>{color}</div>
+            <div className='color' key={color} onClick={() => onClick(color)}>{color}</div>
         ))}
     </div>
   )
 }
 
 Colors.propTypes = {
-    colors: PropTypes.any
+    colors: PropTypes.any,
+    onClick: PropTypes.any
 }
 
 export default Colors
--- src/components/ItemCreate.jsx
+++ src/components/ItemCreate.jsx
@@ -3,25 +3,41 @@ import items from '../assets/items.json'
 import Item from './Item'
 import Colors from "./Colors";
 import './ItemCreate.css'
+import { useState } from "react";
 
 
 const ItemCreate = () => {
     let params = useParams();
     const currentItem = items.find((item) => item.id === params.id)
+
+    const [colorList, setColorList] = useState([]);
+
+    const handleAddColor = (color) => {
+      setColorList([...colorList, color]);
+    }
+
   return (
     <>
         <div>Current URL ID: {params.id}</div>
         {currentItem
-        ? (<div>Current Item: {currentItem.title} the category is {currentItem.category} Testing: {currentItem.colors[1]}</div>)
+        ? (
+        <>
+          <div>Current Item: {currentItem.title} the category is {currentItem.category} Testing: {currentItem.colors[1]}</div>
+          <Item
+            title={currentItem.title}
+            img={currentItem.img}
+            className='item'/>
+          <Colors
+            colors={currentItem.colors}
+            onClick={handleAddColor}/>
+            <div>
+              {colorList.map((color) => (
+                <p key={color}>{color}</p>
+              ))}
+            </div>
+        </>
+        )
         : (<div>No item found</div>)}
-        <Item
-        title={currentItem.title}
-        img={currentItem.img}
-        className='item'/>
-        <Colors
-        colors={currentItem.colors}
-        />
-
     </>
   )
 }
