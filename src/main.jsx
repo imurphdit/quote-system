ba2c0f992ef70f945044b419c3638eea5e5bba89
imurphdit <masinimurphy@icloud.com>
2025-01-28 19:28:59 -0500

    cleaned up code

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import ItemCreate from './components/ItemCreate.jsx'
+import items from './assets/items.json'
+import ItemList from './components/ItemList.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<ItemList items={items} />} />
         <Route path="item/:id" element={<ItemCreate/>}/>
       </Routes>
     </BrowserRouter>
